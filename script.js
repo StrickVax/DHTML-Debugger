cd6e2c037923a28d62d5ad629e7361ca6207e6be
StrickVax <strickvax@gmail.com>
2023-07-17 23:49:23 -0700

    Added tabs

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+// Tab logic
+function openTab(event, tabName) {
+    var i, tabContent, tabLinks;
+
+    tabContent = document.getElementsByClassName('tabContent');
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = 'none';
+    }
+
+    tabLinks = document.getElementsByClassName('tablinks');
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(' active', '');
+    }
+
+    document.getElementById(tabName).style.display = 'block';
+    event.currentTarget.className += ' active';
+}
+
 window.onload = function () {
     document.getElementById('execute-button').onclick = function () {
-
         var code = document.getElementById('code-input').value;
 
         try {
@@ -11,8 +28,4 @@ window.onload = function () {
             }
         }
     };
-
-    // Tab logic
-    
-
-}
+};
